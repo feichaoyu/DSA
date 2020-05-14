c7cd00c8a369cf676eb8b8b926d0a8cfd166cf6b
feichaoyu <42114893+FeiChaoyu@users.noreply.github.com>
2020-05-14 22:40:41 +0800

    剑指offer刷题22-24

---
+++ src/剑指offer/_23_EntryNodeInListLoop/ListNode.java
@@ -0,0 +1,13 @@
+package 剑指offer._23_EntryNodeInListLoop;
+
+/**
+ * @author feichaoyu
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/剑指offer/_24_ReverseList/ListNode.java
@@ -0,0 +1,13 @@
+package 剑指offer._24_ReverseList;
+
+/**
+ * @author feichaoyu
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
