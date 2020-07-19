7858b227abb68820e4b7333a4184b00da810dbc9
feichaoyu <42114893+FeiChaoyu@users.noreply.github.com>
2020-07-19 21:24:57 +0800

    modify

--- src/练习题/队列/LRU2.java
+++ src/练习题/队列/LRU2.java
@@ -1,103 +1,34 @@
 package 练习题.队列;
 
-import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
- * 哈希表+双向链表
- * 还可以参考https://leetcode-cn.com/problems/lru-cache/solution/lru-ce-lue-xiang-jie-he-shi-xian-by-labuladong/
+ * Based on LinkedHashMap
  */
-public class LRU2 {
+public class LRU2 extends LinkedHashMap<Integer, Integer> {
 
-    class DLinkedNode {
-        int key;
-        int value;
-        DLinkedNode prev;
-        DLinkedNode next;
-    }
+    /**
+     * 缓存大小
+     */
+    private int cacheSize;
 
-    private void addNode(DLinkedNode node) {
-        node.prev = head;
-        node.next = head.next;
-
-        head.next.prev = node;
-        head.next = node;
-    }
-
-    private void removeNode(DLinkedNode node) {
-        DLinkedNode prev = node.prev;
-        DLinkedNode next = node.next;
-
-        prev.next = next;
-        next.prev = prev;
-    }
-
-    private void moveToHead(DLinkedNode node) {
-        removeNode(node);
-        addNode(node);
-    }
-
-    private DLinkedNode popTail() {
-        DLinkedNode res = tail.prev;
-        removeNode(res);
-        return res;
-    }
-
-    private Hashtable<Integer, DLinkedNode> cache =
-            new Hashtable<Integer, DLinkedNode>();
-    private int size;
-    private int capacity;
-    private DLinkedNode head, tail;
-
-    public LRU2(int capacity) {
-        this.size = 0;
-        this.capacity = capacity;
-
-        head = new DLinkedNode();
-        // head.prev = null;
-
-        tail = new DLinkedNode();
-        // tail.next = null;
-
-        head.next = tail;
-        tail.prev = head;
+    public LRU2(int cacheSize) {
+        super(cacheSize, 0.75f, true);
+        this.cacheSize = cacheSize;
     }
 
     public int get(int key) {
-        DLinkedNode node = cache.get(key);
-        if (node == null) {
-            return -1;
-        }
-
-        // move the accessed node to the head;
-        moveToHead(node);
-
-        return node.value;
+        return super.getOrDefault(key, -1);
     }
 
     public void put(int key, int value) {
-        DLinkedNode node = cache.get(key);
+        super.put(key, value);
+    }
 
-        if (node == null) {
-            DLinkedNode newNode = new DLinkedNode();
-            newNode.key = key;
-            newNode.value = value;
-
-            cache.put(key, newNode);
-            addNode(newNode);
-
-            ++size;
-
-            if (size > capacity) {
-                // pop the tail
-                DLinkedNode tail = popTail();
-                cache.remove(tail.key);
-                --size;
-            }
-        } else {
-            // update the value.
-            node.value = value;
-            moveToHead(node);
-        }
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > cacheSize;
     }
 
     public void print() {
@@ -105,6 +36,7 @@ public class LRU2 {
     }
 
     public static void main(String[] args) {
+
         LRU2 cache = new LRU2(2);
         cache.put(1, 1);
         cache.put(2, 2);
@@ -120,4 +52,3 @@ public class LRU2 {
         cache.print();                          // {3=3, 4=4}
     }
 }
-
